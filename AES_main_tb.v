92c9b9eb5ff36ba7175e49d42b0f3fc9ca3c8ab1
Prabhat <prabhat.lankireddy@gmail.com>
2019-11-25 17:07:56 +0530

    Updated AES_main.v and AES_main_tb.v
    Made changes to comments and indentation.
    Replaced output 'finish' with 'ready'.

--- AES_main_tb.v
+++ AES_main_tb.v
@@ -2,10 +2,10 @@ module AES_main_tb;
    reg          reset, clk;
    reg [127:0]  i_block, key;
    wire [127:0] o_block;
-   wire         finish;
+   wire         ready;
 
 
-   AES_main AES(.i_block(i_block), .clk(clk), .init_key(key), .reset(reset), .o_block(o_block), .block_finish(finish));
+   AES_main AES(.i_block(i_block), .clk(clk), .init_key(key), .reset(reset), .o_block(o_block), .ready_for_input(ready));
    initial
      begin
         reset = 1;
