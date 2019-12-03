8488a913d2aa755d44d79dd02c17705228edf36c
Prabhat <prabhat.lankireddy@gmail.com>
2019-12-03 19:15:28 +0530

    Created 'Sequential' and 'Combinational' folders
    These folders contain the respective versions of AES.
    
    The combinational folder needs to be updated.

--- Combinational/ByteSubstitution.v
+++ Combinational/ByteSubstitution.v
@@ -1,6 +1,6 @@
 module ByteSubstitution(data, scramble);
 
-//Byte Substitution is the first step to encrypt the information. 
+//Byte Substitution is the first step to encrypt the information.
 //The 128 bit data is divided into a 4x4 matrix where each element has 1 byte i.e. 8 bis of information.
 
 input [127:0] data;
@@ -8,10 +8,10 @@ output [127:0] scramble;
 
 // The data is scrambled with respect to the S-Box byte-by-byte.
 	sbox s0(.a(data[7:0]),.c(scramble[7:0]) );
-	sbox s1(.a(data[15:8]),.c(scramble[15:8]) ); 
+	sbox s1(.a(data[15:8]),.c(scramble[15:8]) );
 	sbox s2(.a(data[23:16]),.c(scramble[23:16]) );
-	sbox s3(.a(data[31:24]),.c(scramble[31:24]) );   
-	sbox s4(.a(data[39:32]),.c(scramble[39:32]) ); 
+	sbox s3(.a(data[31:24]),.c(scramble[31:24]) );
+	sbox s4(.a(data[39:32]),.c(scramble[39:32]) );
 	sbox s5(.a(data[47:40]),.c(scramble[47:40]) );
 	sbox s6( .a(data[55:48]),.c(scramble[55:48]) );
 	sbox s7( .a(data[63:56]),.c(scramble[63:56]) );
@@ -23,6 +23,6 @@ output [127:0] scramble;
 	sbox s13( .a(data[111:104]),.c(scramble[111:104]) );
 	sbox s14( .a(data[119:112]),.c(scramble[119:112]) );
 	sbox s15( .a(data[127:120]),.c(scramble[127:120]) );
-     
+
 
 endmodule
