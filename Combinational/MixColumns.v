541d58f01ce8d8acb21165cf35d4d95e51fb84db
Jayyyoooooo <57684106+Jayyyoooooo@users.noreply.github.com>
2019-12-04 12:06:21 +0530

    Add files via upload
    Modularized Mixed columns and RoundkeyGenerator

--- Combinational/MixColumns.v
+++ Combinational/MixColumns.v
@@ -1,59 +1,61 @@
-module MixColumns(
-		 input wire [127:0] i_block;
-		 output wire [127:0] o_block;
-		 );
-  
-   reg [31:0] w0,w1,w2,w3;
-   reg [31:0] mw0,mw1,mw2,mw3;
-   reg [127:0] o_block_reg;
-
-   assign o_block = o_block_reg;
-
-   initial begin
-	    w0 = block[127:96];
-	    w1 = block[95:64];
-	    w2 = block[63:32];
-	    w3 = block[31:0];
-
-	    mw0 = MixW(w0);
-	    mw1 = MixW(w1);
-	    mw2 = MixW(w2);
-	    mw3 = MixW(w3);
-
-	    o_block_reg = {mw0, mw1, mw2, mw3};
-   end
+module MixColumns(input wire [127:0] i_block, output reg [127:0] o_block);
    
    function [7:0] gm2(input [7:0] a);
       begin
-	       gm2 = {a[6:0],1'b0} ^ (8'h1b & {8{a[7]}});
+	 gm2 = {a[6:0],1'b0} ^ (8'h1b & {8{a[7]}});
       end
    endfunction // gm2
 
    
    function [7:0] gm3(input a);
       begin
-	       gm3 = gm2(a) ^ a;
+	 gm3 = gm2(a) ^ a;
       end
    endfunction // gm3
    
    
-   function [31:0] MixW(input [31:0] col);
+   function [31:0] MixWords(input [31:0] col);
       reg   [7:0] b0,b1,b2,b3;
-      reg [7:0]   mb0, mb1, mb2, mb3;
+      reg   [7:0] mb0, mb1, mb2, mb3;
       begin
-	       b0 = col[31:24];
-	       b1 = col[23:16];
-	       b2 = col[15:8];
-	       b3 = col[7:0];
+	 b3 = col[31:24];
+	 b2 = col[23:16];
+	 b1 = col[15:8];
+	 b0 = col[7:0];
 
-	       mb0 = gm2(b0) ^ gm3(b1) ^ b2 ^ b3;
-	       mb1 = b0 ^ gm2(b1) ^ gm3(b2) ^ b3;
-	       mb2 = b0 ^ b1 ^ gm2(b2) ^ gm3(b3);
-	       mb3 = gm3(b0) ^ b1 ^ b2 ^ gm2(b3);
+	 mb0 = gm2(b0) ^ gm3(b1) ^ b2 ^ b3;
+	 mb1 = b0 ^ gm2(b1) ^ gm3(b2) ^ b3;
+	 mb2 = b0 ^ b1 ^ gm2(b2) ^ gm3(b3);
+	 mb3 = gm3(b0) ^ b1 ^ b2 ^ gm2(b3);
 
-	       MixWords = {mb0, mb1, mb2, mb3};
+	 MixWords = {mb0, mb1, mb2, mb3};
+	 
       end
+      
    endfunction // MixWords
+
+   function [127:0] MC(input [127:0] block);
+      reg [31:0] w0,w1,w2,w3;
+      reg [31:0] mw0,mw1,mw2,mw3;
+      begin
+	 w3 = block[127:96];
+	 w2 = block[95:64];
+	 w1 = block[63:32];
+	 w0 = block[31:0];
+
+	 mw0 = MixWords(w0);
+	 mw1 = MixWords(w1);
+	 mw2 = MixWords(w2);
+	 mw3 = MixWords(w3);
+
+	 MC = {mw3, mw2, mw1, mw0};
+	 
+      end
+   endfunction // MixColumns
+
+always @(i_block) begin
+ assign  o_block = MC(i_block);
+end
 
 
 endmodule
--- Combinational/RoundKeyGenerator.v
+++ Combinational/RoundKeyGenerator.v
@@ -1,39 +1,28 @@
-module RoundKeyGenerator(rc,inkey,outkey)
-  input[3:0] rc;
-  input[127:0] inkey;
-  output[127:0] outkey;
+module RoundKeyGenerator(rc,inkey,outkey);
+  input [3:0] rc;
+  input [127:0] inkey;
+  output [127:0] outkey;
 
   //Assigning words according to grouping.
   wire [31:0] w0,w1,w2,w3,tem;
   assign w0=inkey[127:96];
   assign w1=inkey[95:64];
   assign w2=inkey[63:32];
-  assign w3=inkey[31:0];//Taking from reverse.
+  assign w3=inkey[31:0];
+  
+  //Taking from reverse.
   //rc represents the round number.
   //There are three steps to calculate first part of outkey:
   //One: Left shift w3 by one byte.
   //Two: Byte Substitution and xor with w0
   //Three: XOR with round constant.
 
-  //Step one and two.
-  sbox s1(.in(w3[23:16]),.out(tem[31:24]));
-  sbox s2(.in(w3[15:8]),.out(tem[23:16]));
-  sbox s3(.in(w3[7:0]),.out(tem[15:8]));
-  sbox s4(.in(w3[31:24]),.out(tem[7:0]));
-
-
-  //Step three and further progress.
-  assign outkey[127:96]= w0^tem^rcon(rc);//The complicated one...
-  assign outkey[95:64]= w0^tem^rcon(rc)^w1;//Simple ones
-  assign outkey[95:64]= w0^tem^rcon(rc)^w1^w2;
-  assign outkey[95:64]= w0^tem^rcon(rc)^w1^w2^w3;
-  
-
 //Function to generate round constant depending on the round number.
 //Can be created as a separate module.
-  function automatic [31:0] Rcon(rc);
-     input [3:0] rc;
-     case(rc)
+
+  function automatic [31:0] Rcon;
+     input [3:0] rc1;
+     case(rc1)
 	4'h0: Rcon=32'h01_00_00_00;//Underscore used to improve readability of bytes.
 	4'h1: Rcon=32'h02_00_00_00;
 	4'h2: Rcon=32'h04_00_00_00;
@@ -46,4 +35,20 @@ module RoundKeyGenerator(rc,inkey,outkey)
 	4'h9: Rcon=32'h36_00_00_00;
 	default: Rcon=32'h00_00_00_00;
       endcase
+
   endfunction
+
+  //Step one and two.
+  sbox s1(.a(w0[7:0]),.c(tem[31:24]));
+  sbox s2(.a(w0[31:24]),.c(tem[23:16]));
+  sbox s3(.a(w0[23:16]),.c(tem[15:8]));
+  sbox s4(.a(w0[15:8]),.c(tem[7:0]));
+
+
+  //Step three and further progress.
+  assign outkey[31:0]   = w3^tem^Rcon(rc);//The complicated one...
+  assign outkey[63:32]  = w3^tem^Rcon(rc)^w2;//Simple ones
+  assign outkey[95:64]  = w3^tem^Rcon(rc)^w2^w1;
+  assign outkey[127:96] = w3^tem^Rcon(rc)^w2^w1^w0;
+
+endmodule
