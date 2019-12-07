286e1d9932bd180ff874b2f07fcfe15e60e1cba7
Sudarshan H V <45071414+SudarshanHV@users.noreply.github.com>
2019-12-07 09:50:34 +0530

    Completed Verifying Test Bench. Additional tests may be done

--- Combinational/AESCipher_tb.v
+++ Combinational/AESCipher_tb.v
@@ -4,22 +4,18 @@ module AesCipher_tb;
    reg          clk;
    reg [127:0]  i_block, key;
    wire [127:0] o_block;
-   initial
-     begin
-	      #1 i_block = 128'h10101010202020203030303040404040;
-	      key = 128'h11111111222222223333333344444444;
-     end
-	
-   
    AesCipher AES(.datain(i_block), .clk(clk), .key(key), .dataout(o_block));
 
-   always
+   
+   initial
      begin
-        #1 clk = ~clk;
+	      i_block = 128'h10101010202020203030303040404040;
+	      key = 128'h11111111222222223333333344444444;
      end
 
    always
      begin
-        #2 i_block = i_block + 1'b1;
+        #1 i_block = i_block + 1'b1;
      end
+
 endmodule
